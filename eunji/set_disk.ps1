898d3565b9435dfc47384881d9235759246fb4d1
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 14:17:39 +0900

    zz

--- eunji/set_disk.ps1
+++ eunji/set_disk.ps1
@@ -4,11 +4,12 @@
 # https://learn.microsoft.com/en-us/powershell/module/storage/get-partition?view=windowsserver2022-ps
 # https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
 # https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
-$disknum = 2
+
+$disknum = 1
 $driveletter = Z
 
 # Set disk online
-Set-disk -Number -IsOffline $false
+Set-disk -Number $disknum -IsOffline $false
 
 # initailize disk partitionstyle
 Initialize-Disk -Number $disknum -PartitionStyle MBR
