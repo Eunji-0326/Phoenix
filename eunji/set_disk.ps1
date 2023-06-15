48f72aba18b5f2c08fa2ce6df47039b83ada21a6
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 14:28:03 +0900

    z

--- eunji/set_disk.ps1
+++ eunji/set_disk.ps1
@@ -5,7 +5,7 @@
 # https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
 # https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
 
-$disknum = "1"
+$disknum = 1
 $driveletter = "Z"
 
 # Set disk online
@@ -14,7 +14,7 @@ Set-disk -Number $disknum -IsOffline $false
 # initailize disk partitionstyle
 Initialize-Disk -Number $disknum -PartitionStyle MBR
 
-New-Partition -DiskNumber $disknum -UseMaximunSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDrive
+New-Partition -DiskNumber $disknum -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDrive
 
 Get-Partition -DiskNumber $disknum | Set-Partition -NewDriveLetter $driveletter
 
