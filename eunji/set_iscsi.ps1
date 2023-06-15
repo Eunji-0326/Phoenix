5f52405050cf180cde1d493408482379a3ec518f
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 14:12:28 +0900

    zz

--- eunji/set_iscsi.ps1
+++ eunji/set_iscsi.ps1
@@ -5,12 +5,7 @@
 # https://learn.microsoft.com/ko-kr/powershell/module/iscsitarget/new-iscsivirtualdisk?view=windowsserver2019-ps
 # https://learn.microsoft.com/ko-kr/powershell/module/iscsitarget/set-iscsiservertarget?view=windowsserver2022-ps
 # https://learn.microsoft.com/ko-kr/powershell/module/iscsitarget/add-iscsivirtualdisktargetmapping?view=windowsserver2019-ps
-# https://learn.microsoft.com/en-us/powershell/module/storage/set-disk?view=windowsserver2022-ps
-# https://learn.microsoft.com/en-us/powershell/module/storage/initialize-disk?view=windowsserver2022-ps
-# https://learn.microsoft.com/en-us/powershell/module/storage/set-partition?view=windowsserver2022-ps
-# https://learn.microsoft.com/en-us/powershell/module/storage/get-partition?view=windowsserver2022-ps
-# https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
-# https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
+
 
 $driveletter = Z
 $targetname = "iscsi-test"
