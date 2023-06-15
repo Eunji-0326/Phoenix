f3760fb5f0e2064aceb9ad820651272af4d0f255
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 08:43:33 +0900

    ss

--- eunji/add_hosts.sh
+++ eunji/add_hosts.sh
@@ -2,4 +2,5 @@
 sudo echo "10.10.13.2 k8s-master" >> /etc/hosts
 sudo echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
 sudo echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
-sudo echo "10.10.13.5 k8s-worker-04" >> /etc/hosts
+sudo echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
+sudo echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
--- eunji/set_k8s_master.sh
+++ eunji/set_k8s_master.sh
@@ -48,3 +48,10 @@ sudo apt install -y iptables-persistent
 
 # k8s host name change
 hostnamectl set-hostname k8s-master
+
+# add hosts file 
+sudo echo "10.10.13.2 k8s-master" >> /etc/hosts
+sudo echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
+sudo echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
+sudo echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
+sudo echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
