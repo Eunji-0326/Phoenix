40b3d01779baae126a880485fd97e8e9e5ba88e6
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 12:21:05 +0900

    zz

--- eunji/set_disk.ps1
+++ eunji/set_disk.ps1
@@ -5,7 +5,8 @@
 # https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
 # https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
 
-$disknum = 1 # or 2
+$disknum = 3 # or 2
+$driveletter = Y
 
 # Set disk online
 Set-disk -Number -IsOffline $false
@@ -14,7 +15,7 @@ Set-disk -Number -IsOffline $false
 Initialize-Disk -Number $disknum -PartitionStyle mbr
 
 Initialize-Disk -DiskNumber $disknum -UseMaximunSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDrive
-Get-Partition -DiskNumber 1 | Set-Partition -NewDriveLetter X
+Get-Partition -DiskNumber 1 | Set-Partition -NewDriveLetter $driveletter
 
 # install iSCSI Target with admin tools
 Install-WindowsFeature FS-iSCSITarget-Server -IncludeManagementTools
