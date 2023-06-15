b2969e72844f678efbe865230ad6a1a57b2dabd3
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 14:23:33 +0900

    ss

--- eunji/set_disk.ps1
+++ eunji/set_disk.ps1
@@ -5,7 +5,7 @@
 # https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
 # https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
 
-$disknum = 1
+$disknum = "1"
 $driveletter = "Z"
 
 # Set disk online
