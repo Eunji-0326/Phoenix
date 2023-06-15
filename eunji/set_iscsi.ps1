7686fdf7083bfed35f6e3c11874edef95bd268e0
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-15 12:45:35 +0900

    test

--- eunji/set_iscsi.ps1
+++ eunji/set_iscsi.ps1
@@ -12,26 +12,10 @@
 # https://learn.microsoft.com/en-us/powershell/module/storage/format-volume?view=windowsserver2022-ps
 # https://tiprelay.com/%EC%9C%88%EB%8F%84%EC%9A%B010%EC%97%90%EC%84%9C-powershell%EB%A1%9C-%EB%93%9C%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%A5%BC-%ED%8F%AC%EB%A7%B7%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
 
-$disknum = 3 # or 2
-$driveletter = Y
+$driveletter = Z
 $targetname = "iscsi-test"
 $diskpath = $driveletter + ":\iSCSIDisk\test-LUN0.vhdx"
 $disksize = 100GB
-
-# Set disk online
-Set-disk -Number -IsOffline $false
-
-# initailize disk partitionstyle
-Initialize-Disk -Number $disknum -PartitionStyle mbr
-
-Initialize-Disk -DiskNumber $disknum -UseMaximunSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel myDrive
-Get-Partition -DiskNumber 1 | Set-Partition -NewDriveLetter $driveletter
-
-# install iSCSI Target with admin tools
-Install-WindowsFeature FS-iSCSITarget-Server -IncludeManagementTools
-
-# restart computer to apply changes
-Restart-Computer -Force 
 
 # create iSCSI Target
 New-IscsiServerTarget -TargetName $targetname -InitiatorId @("IPAddress:10.10.11.14","IPAddress:10.10.11.15","IPAddress:10.10.11.16")
