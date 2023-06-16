57c40ee4cf026000ccb347dac146543def2aaf23
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 10:50:21 +0900

    zz

--- eunji/set_k8s_master.sh
+++ eunji/set_k8s_master.sh
@@ -54,4 +54,7 @@ echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
 echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
 echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
 echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
+
+# set korean
+gsettings set org.gnome.desktop.input-sources sources "[('ibus', 'hangul')]"
 EOF
