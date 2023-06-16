6fceee5ab1f3769d06c38c6fbcdfecd0ea57ead3
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 15:28:13 +0900

    install k8s

--- eunji/add_hosts.sh
+++
@@ -1,6 +0,0 @@
-# add hosts file 
-echo "10.10.13.2 k8s-master" >> /etc/hosts
-echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
-echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
-echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
-echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
--- eunji/install_k8s.sh
+++ eunji/install_k8s.sh
@@ -1,53 +1,38 @@
 # swap disable
-sudo swapoff -a
+swapoff -a
 
 # firewall disable
-sudo ufw disable
+ufw disable
 
-# lock 걸림 방지
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/lib/dpkg/lock
+# lock prevent
+rm -rf /var/lib/dpkg/lock-frontend
+rm -rf /var/lib/dpkg/lock
 
-# https로 repository 접근 후 사용 가능하도록 패키지 다운로드
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+# set network
+echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/k8s.conf
+echo "tee /etc/sysctl.d/k8s.conf" >> /etc/modules-load.d/modules.conf
+echo "br netfilter" >> /etc/modules-load.d/modules.conf
 
-# 네트워크 설정
-sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-ipta
-EOFbles = 1
+# Kubernetes public GPG key add
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-sudo tee /etc/modules-load.d/modules.conf
-br netfilter
-EOF
+# Kubernetes package store add
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
-# Kubernetes  공식 GPG 키 추가
-sudo  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# k8s command install
+apt install -y kubelet kubeadm kubectl
 
-# Kubernetes 패키지 저장소 추가
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# k8s version fix
+apt-mark hold kubelet kubeadm kubectl
 
-# 패키지 업데이트
-sudo apt-get update
-
-# 다운로드
-sudo apt install -y kubelet kubeadm kubectl
-
-# lock 걸림 방지
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/lib/dpkg/lock
-
-# 쿠버네티스 버전 고정
-sudo apt-mark hold kubelet kubeadm kubectl
-
-# 데몬 재시작
+# daemon restart
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
-# docker 관련 파일 설정
-sudo mkdir /etc/docker
+# docker directory create
+mkdir /etc/docker
 
-cat <<EOF | sudo tee /etc/docker/daemon.json
+cat <<EOF | tee /etc/docker/daemon.json
 {
 "exec-opts": ["native.cgroupdriver=systemd"],
 "log-driver": "json-file",
@@ -58,14 +43,10 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
-# docker 및 관련 데몬 재시작
-sudo systemctl enable docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+# docker restart
+systemctl daemon-reload
+systemctl restart docker
 
-# kubelet 실행
-sudo systemctl start kubelet
-sudo systemctl enable kubelet
-
-# root 계정으로 변경
-sudo su -
+# kubelet startm enable
+systemctl start kubelet
+systemctl enable kubelet
