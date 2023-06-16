4623ccc10f792754595b22f466288d901bec07a4
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 09:46:50 +0900

    change

--- eunji/add_hosts.sh
+++ eunji/add_hosts.sh
@@ -1,6 +1,6 @@
 # add hosts file 
-sudo echo "10.10.13.2 k8s-master" >> /etc/hosts
-sudo echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
-sudo echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
-sudo echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
-sudo echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
+echo "10.10.13.2 k8s-master" >> /etc/hosts
+echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
+echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
+echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
+echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
