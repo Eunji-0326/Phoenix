dd6970af255a2931fc59b35440ca79a8e17a894a
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 10:27:30 +0900

    ss

--- eunji/set_k8s_master.sh
+++ eunji/set_k8s_master.sh
@@ -7,6 +7,24 @@ VMware1!
 EOF
 echo "Root password changed successfully."
 
+# set IP address
+network_config=$(cat <<EOL
+network:
+ version: 2
+ renderer: NetworkManager
+ ethernets:
+  ens192:
+   dhcp4: no
+   addresses:
+    - 10.10.13.2/24
+   gateway4: 10.10.13.1
+   nameservers:
+    addresses: [10.10.10.2, 203.248.252.2]
+EOL
+)
+echo "$network_config" | sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null
+sudo netplan apply
+
 # repository update
 # sudo systemctl stop unattended-upgrades.service
 sudo apt update -y
