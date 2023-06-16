3e103c076955b3a71f11cbc50e80282c0c28a884
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 10:46:48 +0900

    ee

--- eunji/set_k8s_master.sh
+++ eunji/set_k8s_master.sh
@@ -25,36 +25,33 @@ network:
 EOL
 )
 echo "$network_config" | sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null
-sudo netplan apply
+netplan apply
 
 # repository update
 # sudo systemctl stop unattended-upgrades.service
-sudo apt update -y
+apt update -y
 
 # ssh server install
-sudo apt install -y openssh-server
+apt install -y openssh-server
 
 # ifconfig command install
-sudo apt install net-tools
+apt install net-tools
 
 # package install
-sudo apt install -y ca-certificates curl gnupg lsb-release
+apt install -y ca-certificates curl gnupg lsb-release
 
 # vim package install
-sudo apt install -y vim
+apt install -y vim
 
 # iptables install
 echo 'iptables-persistent iptables-persistent/autosave_v4 boolean true' | sudo debconf-set-selections
 echo 'iptables-persistent iptables-persistent/autosave_v6 boolean false' | sudo debconf-set-selections
-sudo apt install -y iptables-persistent
-
-# k8s host name change
-hostnamectl set-hostname k8s-master
+apt install -y iptables-persistent
 
 # add hosts file 
-sudo echo "10.10.13.2 k8s-master" >> /etc/hosts
-sudo echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
-sudo echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
-sudo echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
-sudo echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
+echo "10.10.13.2 k8s-master" >> /etc/hosts
+echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
+echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
+echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
+echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
 EOF
