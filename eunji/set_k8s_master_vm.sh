5470669665ec4491cfef09ad2e5a400570b82460
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 16:20:24 +0900

    clone vm

--- eunji/set_k8s_master_vm.sh
+++ eunji/set_k8s_master_vm.sh
@@ -38,7 +38,6 @@ echo "10.10.13.2 k8s-master" >> /etc/hosts
 echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
 echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
 echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
-echo "10.10.13.6 k8s-worker-04" >> /etc/hosts
 
 # set korean
 gsettings set org.gnome.desktop.input-sources sources "[('ibus', 'hangul')]"
