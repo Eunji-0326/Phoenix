6695603cdf22eff8e45e31bef985fcba8de8a96b
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-16 23:04:21 +0900

    ss

--- eunji/set_k8s_worke_02.sh
+++ eunji/set_k8s_worke_02.sh
@@ -2,6 +2,7 @@
 
 su - << EOF
 VMware1!
+hostnamectl set-hostname k8s-worker-02
 cat /home/phoenix/set_net.yml > /etc/netplan/01-network-manager-all.yaml
 netplan apply
 EOF
---
+++ eunji/set_k8s_worker_01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+su - << EOF
+VMware1!
+hostnamectl set-hostname k8s-worker-01
+cat /home/phoenix/set_net.yml > /etc/netplan/01-network-manager-all.yaml
+netplan apply
+EOF
