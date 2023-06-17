163f1218ea942ef798f5c98a3c42c910afcf4b7a
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-18 01:55:14 +0900

    ss

--- eunji/set_k8s_master.sh
+++ eunji/set_k8s_master.sh
@@ -32,14 +32,14 @@ sha256_hash=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa 
 echo "$sha256_hash" >> /home/phoenix/token/token.txt
 
 
-# 토큰을 worker로 복사
-apt install -y expect
+# # 토큰을 worker로 복사
+# apt install -y expect
 
-expect << EOF
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.3:/home/phoenix/token/token.txt
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.4:/home/phoenix/token/token.txt
-spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.5:/home/phoenix/token/token.txt
+# expect << EOF
+# spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.3:/home/phoenix/token/token.txt
+# spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.4:/home/phoenix/token/token.txt
+# spawn scp -o "StrictHostKeyChecking=no" /home/phoenix/token/token.txt phoenix@10.10.13.5:/home/phoenix/token/token.txt
 
-expect "password:" {send "VMware1!\r"}
-expect eof
-EOF
+# expect "password:" {send "VMware1!\r"}
+# expect eof
+# EOF
