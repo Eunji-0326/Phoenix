73a93aa0751baeaefe9a6a990f4ebc6f32115195
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-18 01:51:37 +0900

    s

--- eunji/set_k8s_worker.sh
+++ eunji/set_k8s_worker.sh
@@ -1,5 +1,5 @@
 # join to master
-token=$(sed -n '1p' /home/test01/token.txt)
-sha256=$(sed -n '2p' /home/test01/token.txt)
+token=$(sed -n '1p' /home/phoenix/token/token.txt)
+sha256=$(sed -n '2p' /home/phoenix/token/token.txt)
 
 kubeadm join 10.10.13.2:6443 --token $token --discovery-token-ca-cert-hash sha256:$sha256
