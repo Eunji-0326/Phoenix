a5a09e2fdfc3caaa58c4c5be9d93af942a3d62c6
Eunji-0326 <dldnjsvlf123@naver.com>
2023-06-23 14:29:13 +0900

    change vm setting

--- eunji/set_k8s_worker_02.sh
+++ eunji/set_k8s_worker_02.sh
@@ -5,5 +5,9 @@ VMware1!
 hostnamectl set-hostname k8s-worker-02
 cat /home/phoenix/set_net.yml > /etc/netplan/01-network-manager-all.yaml
 netplan apply
-echo "127.0.0.1 k8s-worker-02" >> /etc/hosts
+echo "127.0.0.1 k8s-worker-02" > /etc/hosts
+echo "10.10.13.2 k8s-master" >> /etc/hosts
+echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
+echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
+echo "10.10.13.5 k8s-worker-03" >> /etc/hosts
 EOF
--- eunji/set_phx_docker.sh
+++ eunji/set_phx_docker.sh
@@ -5,7 +5,7 @@ VMware1!
 hostnamectl set-hostname k8s-worker-01
 cat /home/phoenix/set_net.yml > /etc/netplan/01-network-manager-all.yaml
 netplan apply
-echo "127.0.0.1 k8s-worker-01" > /etc/hosts
+echo "127.0.0.1 phx-docker" > /etc/hosts
 echo "10.10.13.2 k8s-master" >> /etc/hosts
 echo "10.10.13.3 k8s-worker-01" >> /etc/hosts
 echo "10.10.13.4 k8s-worker-02" >> /etc/hosts
